7401b8bdffef3e9d3014c22992cbf412a853a422
David Castillo <hola@davecastworks.com>
2020-07-27 00:17:09 -0400

    Delete files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState } from "react";
+
+/*COMPONENTS*/
+import Header from "./components/Header";
+import Form from "./components/Form";
+import Summary from "./components/Summary";
 
 function App() {
+  const [summary, setSummary] = useState({
+    quotation: 0,
+    information: {
+      brand: '',
+      year: '',
+      plan: ''
+    }
+  })
+
+  const { information } = summary;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Header title="Cotizador de Seguros" />
+      <Form 
+        setSummary={setSummary}
+      />
+      <Summary
+        information={information}
+      />
+    </Fragment>
   );
 }
 
